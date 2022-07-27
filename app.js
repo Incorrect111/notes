2db59fdc8693680bf6d9332d495b249ca211b58d
Incorrect111 <alexeev.andrey1987@gmail.com>
2022-07-27 18:10:38 +0300

    final commit

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const App = {
       title: 'Notes',
       input: {
         value: '',
-        placeholder: 'Type your note'
+        placeholder: 'Type your note',
       },
-      notes: []
+      notes: [],
     }
   },
 
@@ -20,25 +20,47 @@ const App = {
   },
 
   mounted() {
-  this.getNotes();
+    this.getNotes();
   },
-
 
   methods: {
     getNotes() {
       const localNotes = localStorage.getItem('notes');
       if (localNotes) {
         this.notes = JSON.parse(localNotes)
+        console.log(this.notes)
       }
     },
+    editNote(id) {
+      const editingNote = this.notes.find(item => item.id === id);
+      editingNote.editing = !editingNote.editing;
+      editingNote.hiddenInput.value = editingNote.value;
+    },
+    submitEditingNote(id) {
+      const editingNote = this.notes.find(item => item.id === id);
+      editingNote.value = editingNote.hiddenInput.value;
+      //close
+      editingNote.editing = false;
+    },
     onSubmit() {
-      this.notes.push(this.input.value)
-      // localStorage.setItem('note', 'hello world')
+      this.notes.push({
+        id: Math.floor((1 + Math.random()) * 0x10000)
+          .toString(16)
+          .substring(1),
+        value: this.input.value,
+        hiddenInput: {
+          value: '',
+          placeholder: 'Redact your note'
+        },
+        editing: false
+      })
       this.input.value = ""
     },
-    remove(index) {
-      console.log(`note ${index} has been removed`)
-      this.notes.splice(index, 1)
+    remove(id) {
+      const index = this.notes.findIndex(n => n.id === id);
+      if (index !== -1) {
+        this.notes.splice(index, 1);
+      }
     }
   }
 }
